e9327a288f032d04d8091887fa172ee2e0d7fef9
Shahar Ilany <31574996+ShaharIlany@users.noreply.github.com>
2024-11-20 21:51:19 +0200

    remove log

--- sync.ts
+++ sync.ts
@@ -132,7 +132,6 @@ export const execute = async () => {
                     return false
                 }
 
-                console.log(`<${uidForLogs}>: ${event.isAllDay} ${format(parseISO(googleEvent.end.dateTime ?? ""), "yyyy-MM-dd'T'HH:mm:ssXXX")} ${format(asiaJerusalem(event.end), "yyyy-MM-dd'T'HH:mm:ssXXX")}`)
                 // If they are not all day events but the end dates are different then those are not the same events
                 if (!event.isAllDay && format(parseISO(googleEvent.end.dateTime ?? ""), "yyyy-MM-dd'T'HH:mm:ssXXX") !== format(asiaJerusalem(event.end), "yyyy-MM-dd'T'HH:mm:ssXXX")) {
                     return false
